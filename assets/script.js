c283b7baf02b60bd40b87548e195598687257fea
Dinimar Fish <tayla@tallisman.com.au>
2021-07-09 22:10:03 +1000

    applying styles to elements and beginning pseudocode

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+// So - there are really 2 main functions happening here.
+// One is the colour change indicating we are behind schedule.
+// As part of the starter code, we've been given classes to indicate
+// at what stage of the project we are at.
+
+//Two is saving the text onto the main page.
+
+//While there is also the date required for the Jumbotron, that in itself
+// is pretty straightforward.
