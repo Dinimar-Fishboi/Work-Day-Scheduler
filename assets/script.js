c9310108668b0a3790388b59fb6ba2782d44dc3a
Dinimar Fish <tayla@tallisman.com.au>
2021-07-10 23:07:00 +1000

    added hh:mm:ss a to continue ticking because we always need a reason to peak our anxious minds

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,20 @@
- //Date for the Jumbotron.
+ //Date and time for the Jumbotron (aka the easy bit)
 
- var currentDay = moment();
- $("#currentDay").text(currentDay.format("dddd, MMMM Do"));
+//  var currentDay = $("#currentDay")
+//  var specificTime = $("#specificTime")
 
- $("#tenAM .activityInput").text("hold me closer tiny dancer");
+//  var currentDay = moment();
+//  $("#currentDay").text(currentDay.format("dddd, MMMM Do"));
+//  $("#specificTime").text(currentDay.format("hh:mm:ss a"))
 
+
+function displayTime() {
+    var currentDay = moment();
+    $("#currentDay").text(currentDay.format("dddd, MMMM Do"));
+    $("#specificTime").text(currentDay.format("hh:mm:ss a"))
+}
+
+setInterval(displayTime, 1000);
 
 //  var eightAM = document.querySelector("#eightAM");
 //  var eightAMInput = document.querySelector("#eightAM #activityInput");
@@ -19,156 +29,37 @@
 // that can be changed depending on what time of the day it is, indicating
 // that we're looking at a conditional statment based on TIME.
 
-//Two is saving the text onto the main page.
-// Now - about that.... really all we are doing is saving the content to
-// the page, regardless of what textarea has been modified.
-// So..... we just need to figure out how to save the page.
+//Two is saving the inputs to the correlating rows (i.e the activity for 4pm is
+// only saved to the textarea in the 4pm row rather than ALL of the texareas.)
 
- //var saveBtn = $("#saveBtn");
-// var activityInput = document.querySelectorAll("#activityInput");
-
-$(document).ready(function () {
+//$(document).ready(function () {
 
 
-    // $("#eightAM .activityInput").val(localStorage.getItem("eightAM"));
-    // $("#nineAM .activityInput").val(localStorage.getItem("nineAM"));
-    // $("#tenAM .activityInput").val(localStorage.getItem("tenAM"));
+    $("#eightAM #activityInput").val(localStorage.getItem("eightAM"));
+    $("#nineAM #activityInput").val(localStorage.getItem("nineAM"));
+    $("#tenAM #activityInput").val(localStorage.getItem("tenAM"));
+    $("#elevenAM #activityInput").val(localStorage.getItem("elevenAM"));
+    $("#twelvePM #activityInput").val(localStorage.getItem("twelvePM"));
+    $("#onePM #activityInput").val(localStorage.getItem("onePM"));
+    $("#twoPM #activityInput").val(localStorage.getItem("twoPM"));
+    $("#threePM #activityInput").val(localStorage.getItem("threePM"));
+    $("#fourPM #activityInput").val(localStorage.getItem("fourPM"));
+    $("#fivePM #activityInput").val(localStorage.getItem("fivePM"));
+    $("#sixPM #activityInput").val(localStorage.getItem("sixPM"));
+    $("#sevenPM #activityInput").val(localStorage.getItem("sevenPM"));
+    $("#eightPM #activityInput").val(localStorage.getItem("eightPM"));
+
 
 
    
         $(".saveBtn").on("click", function() {
             console.log("have pressed a button");
-           // var timeDue = activityInput.parent().attr("id");
-          //  console.log(timeDue);
             var updateCalendar = $(this).siblings("#activityInput").val();
             console.log(updateCalendar)
             var hourTime = $(this).parent().attr("id");
             localStorage.setItem(hourTime,updateCalendar);
             console.log(hourTime)
-          //  console.log(updateCalendar);
-
         })
 
-
-
-        $("#eightAM #activityInput").val(localStorage.getItem("eightAM"));
-        $("#nineAM #activityInput").val(localStorage.getItem("nineAM"));
-        $("#tenAM #activityInput").val(localStorage.getItem("tenAM"));
-
-       // console.log($("#eightAM .activityInput").val(localStorage.getItem("eightAM")))
-
-       $("#test").val(localStorage.getItem("eightAM"));
-
-})
-
-// var calendarUpdate = localStorage.getItem("calendar");
-
-// activityInput.textContent = calendarUpdate;
-
-// updateCalendar();
-
-// function updateCalendar() {
-//     var textareaSubmit = localStorage.getItem('key');
-//     activityInput.textContent = textareaSubmit
-// }
-
-// console.log(activityInput)
-// console.log(saveBtn)
-
-// We need to apply the setItem code above the save.Btn work, else nothing will
-// be summoned.
-
-// The following code block ensures that when ANY save button is pressed, the
-// action will be the same.
-
-// saveBtn.forEach(function(){
-//     if ( 54 > 7) {
-//         console.log("correct")
-//     }
-//     for (i = 0 ; i < saveBtn.length; i++ ) {
-
-//         saveBtn[i].addEventListener("click", function(){
-
-//             console.log("saveBtn has been clicked");
-            
-
-//             activityInput[i].textContent = activityInput[i]
-
-//             //calendarUpdate = localStorage.setItem("calendar",JSON.stringify(calendarUpdate));
-
-//         })
-//     }    
-// });
-
-
-// //=====================
-// var emailInput = document.querySelector('#email');
-// var passwordInput = document.querySelector('#password');
-// var signUpButton = document.querySelector('#sign-up');
-// var msgDiv = document.querySelector('#msg');
-// var userEmailSpan = document.querySelector('#user-email');
-// var userPasswordSpan = document.querySelector('#user-password');
-
-// renderLastRegistered();
-
-// function displayMessage(type, message) {
-//   msgDiv.textContent = message;
-//   msgDiv.setAttribute('class', type);
-// }
-
-// function renderLastRegistered() {
-//   var email = localStorage.getItem('email');
-//   var password = localStorage.getItem('password');
-
-//   if (!email || !password) {
-//     return;
-//   }
-
-//   userEmailSpan.textContent = email;
-//   userPasswordSpan.textContent = password;
-// }
-
-// signUpButton.addEventListener('click', function (event) {
-//   event.preventDefault();
-
-//   var email = document.querySelector('#email').value;
-//   var password = document.querySelector('#password').value;
-
-//   if (email === '') {
-//     displayMessage('error', 'Email cannot be blank');
-//   } else if (password === '') {
-//     displayMessage('error', 'Password cannot be blank');
-//   } else {
-//     displayMessage('success', 'Registered successfully');
-
-//     localStorage.setItem('email', email);
-//     localStorage.setItem('password', password);
-//     renderLastRegistered();
-//   }
-// });
-
-
-
-
-
-
-
-// if (saveBtn.addEventListener("click",function(){
-
-//     console.log("yes")
-// }));
-
-     
-// function proofOfConcept() {
-
-//     if ( 54 > 7) {
-//         console.log("correct")
-//     }
-
-//     activityInput.textContent = calendarUpdate;
-
-//     localStorage.setItem("calendar", calendarUpdate);
-  
-// };
-
+//  })
 
