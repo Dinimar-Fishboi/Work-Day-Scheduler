e9b6577a972d34262958908bdc7de49ef0ec65cf
Dinimar Fish <tayla@tallisman.com.au>
2021-07-11 19:16:10 +1000

    neatened js file

--- assets/script.js
+++ assets/script.js
@@ -48,43 +48,40 @@ hourNow();
 //Two is saving the inputs to the correlating rows (i.e the activity for 4pm is
 // only saved to the textarea in the 4pm row rather than ALL of the texareas.)
 
-    //First off, we summon any inputs that may have been entered previously.
+//First off, we summon any inputs that may have been entered previously.
 
-    $("#08 #activityInput").val(localStorage.getItem("08"));
-    $("#09 #activityInput").val(localStorage.getItem("09"));
-    $("#10 #activityInput").val(localStorage.getItem("10"));
-    $("#11 #activityInput").val(localStorage.getItem("11"));
-    $("#12 #activityInput").val(localStorage.getItem("12"));
-    $("#13 #activityInput").val(localStorage.getItem("13"));
-    $("#14 #activityInput").val(localStorage.getItem("14"));
-    $("#15 #activityInput").val(localStorage.getItem("15"));
-    $("#16 #activityInput").val(localStorage.getItem("16"));
-    $("#17 #activityInput").val(localStorage.getItem("17"));
-    $("#18 #activityInput").val(localStorage.getItem("18"));
-    $("#19 #activityInput").val(localStorage.getItem("19"));
-    $("#20 #activityInput").val(localStorage.getItem("20"));
-    $("#21 #activityInput").val(localStorage.getItem("21"));
-    $("#22 #activityInput").val(localStorage.getItem("22"));
+$("#08 #activityInput").val(localStorage.getItem("08"));
+$("#09 #activityInput").val(localStorage.getItem("09"));
+$("#10 #activityInput").val(localStorage.getItem("10"));
+$("#11 #activityInput").val(localStorage.getItem("11"));
+$("#12 #activityInput").val(localStorage.getItem("12"));
+$("#13 #activityInput").val(localStorage.getItem("13"));
+$("#14 #activityInput").val(localStorage.getItem("14"));
+$("#15 #activityInput").val(localStorage.getItem("15"));
+$("#16 #activityInput").val(localStorage.getItem("16"));
+$("#17 #activityInput").val(localStorage.getItem("17"));
+$("#18 #activityInput").val(localStorage.getItem("18"));
+$("#19 #activityInput").val(localStorage.getItem("19"));
+$("#20 #activityInput").val(localStorage.getItem("20"));
+$("#21 #activityInput").val(localStorage.getItem("21"));
+$("#22 #activityInput").val(localStorage.getItem("22"));
 
-    // Here is where the setItem code is. the first line indidcates that the function
-    // is to execute whenever a save button has been pressed.
-   
-        $(".saveBtn").on("click", function() {
-            console.log("have pressed a button");
+// Here is where the setItem code is. the first line indidcates that the function
+// is to execute whenever a save button has been pressed.
 
-            // THIS in the next few lines refers to the button pressed. We are specifically
-            // requesting the sibling with the #activityInput id (aka our textarea element)
-            // and the parent ID of the button (aka 8AM for example).
+    $(".saveBtn").on("click", function() {
+        console.log("have pressed a button");
 
-            //Once those two pieces of data have variables assigned, then they are then placed
-            // in  the local storage under unique keys which can be summoned above.
+        // THIS in the next few lines refers to the button pressed. We are specifically
+        // requesting the sibling with the #activityInput id (aka our textarea element)
+        // and the parent ID of the button (aka 8AM for example).
 
-            var updateCalendar = $(this).siblings("#activityInput").val();
-            console.log(updateCalendar)
-            var hourTime = $(this).parent().attr("id");
-            localStorage.setItem(hourTime,updateCalendar);
-            console.log(hourTime)
-       })
+        //Once those two pieces of data have variables assigned, then they are then placed
+        // in  the local storage under unique keys which can be summoned above.
 
-
-    
+        var updateCalendar = $(this).siblings("#activityInput").val();
+        console.log(updateCalendar)
+        var hourTime = $(this).parent().attr("id");
+        localStorage.setItem(hourTime,updateCalendar);
+        console.log(hourTime)
+    })
